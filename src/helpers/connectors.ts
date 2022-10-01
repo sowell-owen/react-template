f024f8d0c6a291c18c115c59994eccebd5258370
sowell-owen <karakulin.toshik@gmail.com>
2022-10-02 00:04:46 +0300

    feat: impl. connect/disconnect metamask functions

---
+++ src/helpers/connectors.ts
@@ -0,0 +1,8 @@
+import { InjectedConnector } from "@web3-react/injected-connector";
+
+import { CHAIN_ID } from "./constants";
+
+export const injected = () =>
+  new InjectedConnector({
+    supportedChainIds: [CHAIN_ID],
+  });
