dd5e94e100364caf8a1a731d269d862abf45a247
sowell-owen <karakulin.toshik@gmail.com>
2022-10-23 20:06:08 +0300

    refactor: rename files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import RenderRoutes from "./routes";
+import RenderRoutes from "./routes/RenderRoutes";
 
 function App() {
   return (
