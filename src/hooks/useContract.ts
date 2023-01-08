255f71a12f134626d2003dbfb1e33ccbcd6f4edb
sowell-owen <karakulin.toshik@gmail.com>
2023-01-08 10:58:13 +0200

    feat: generic types for useContract hook

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -3,7 +3,10 @@ import { ethers } from "ethers";
 import { ContractNames } from "../helpers/contractNames";
 import * as factories from "../typechain/factories";
 
-const useContract = (contractAddress: string, contractName: ContractNames) => {
+
+// Usage example:
+// const contract = useContract<ContractType>(CONTRACT_ADDRESS, ContractNames.ContractName);
+function useContract<T>(contractAddress: string, contractName: ContractNames): T | undefined {
     const [contract] = Object.entries(factories)
         .map(([key, value]) => {
                 if (key === `${contractName}__factory`) return value;
@@ -13,7 +16,7 @@ const useContract = (contractAddress: string, contractName: ContractNames) => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
 
-    return contract?.connect(contractAddress, signer || provider);
-};
+    return contract ? contract.connect(contractAddress, signer) as T : undefined;
+}
 
 export default useContract;
